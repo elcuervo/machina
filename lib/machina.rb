db5d99a985655643f1b8610272dbab46e001d82b
elcuervo <elcuervo@elcuervo.net>
2021-02-04 11:52:22 -0300

    Some yard fun

--- lib/machina.rb
+++ lib/machina.rb
@@ -8,11 +8,22 @@ class Machina
 
   attr_reader :state, :events
 
+  # Initializes Machina
+  #
+  # @param initial_state [Symbol] the initial state of the state machine
+  # @return [Machina] the initialized Machina
+  #
   def initialize(initial_state)
     @state = initial_state
     @events = Hash.new { |h, k| h[k] = [] }
   end
 
+  # Triggers an event.
+  #
+  # @param event [Symbol] the event to trigger
+  # @param args [Array<Object>] the objects that the callback will receive
+  # @return [void]
+  #
   def trigger(event, *args)
     return unless trigger?(event)
 
@@ -29,26 +40,51 @@ class Machina
     on[event].call(*args)
   end
 
+  # Triggers an event and raises on error.
+  #
+  # @param event [Symbol] the event to trigger
+  # @param args [Array<Object>] the objects that the callback will receive
+  # @return [void]
+  #
   def trigger!(event, *args)
     raise InvalidStateChangeError, "Event #{event} missing #{state} step" unless trigger?(event)
 
     trigger(event, *args)
   end
 
+  # Checks if an event can be triggered
+  #
+  # @param event [Symbol] the event to trigger
+  # @return [Boolean] the chance posibility
+  #
   def trigger?(event)
     raise InvalidEventError, "Event #{event} not found." unless events.key?(event)
 
     events[event].key?(state)
   end
 
+  # Defines the list of possible callbacks when an event is triggered
+  #
+  # @return [Hash]
+  #
   def on
     @on ||= Hash.new(&EVENT)
   end
 
+  # Defines the list of possible callbacks when a state is reached
+  #
+  # @return [Hash]
+  #
   def when
     @when ||= Hash.new(&EVENT)
   end
 
+  # Defines the list of possible events and their transitions
+  #
+  # @param key [Symbol] the event name
+  # @param states [Hash] the event transitions
+  # @return [void]
+  #
   def []=(key, states)
     events[key] = states
   end
